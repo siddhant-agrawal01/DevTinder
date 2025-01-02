8ca63aed132d821c40d1d1eb1e67ecbfc635d56f
Siddhant <siddhant_010@outlook.com>
2025-01-02 23:29:54 +0530

    added alidation in schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
+    required:true,
   },
   password: {
     type: String,
+    required:true,
   },
   age: {
     type: Number,
