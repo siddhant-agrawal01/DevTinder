044c953b20b1c377de4bf4b406832518bce0a56b
Siddhant <siddhant_010@outlook.com>
2025-01-02 23:46:55 +0530

    added alidation in schema2

--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,36 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required:true,
+    required: true,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
-    required:true,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
-    required:true,
+    required: true,
   },
   age: {
     type: Number,
   },
 
   gender: {
+    type: String,
+  },
+  photoUrl: {
+    type: String,
+    default:
+      "https://media.istockphoto.com/id/587805156/vector/profile-picture-vector-illustration.jpg?s=612x612&w=0&k=20&c=gkvLDCgsHH-8HeQe7JsjhlOY6vRBJk_sKW9lyaLgmLo=",
+  },
+  skills: {
+    type: [string],
+  },
+  about: {
     type: String,
   },
 });
